dfba2b3c2315dd99c298a1d6af79780cf0f9917a
pallavi-vetal <vetalpallavi21@gmail.com>
2020-05-21 11:28:11 +0530

    grdfs changes

--- components/event/event.DAL.js
+++ components/event/event.DAL.js
@@ -20,27 +20,30 @@ exports.uploadImage = (req, res) => {
         const fileName = req.files.file.name
         uploadFile.mv(
             `${fileName}`,
-            async function (err) {
+            async (err) => {
                 if (err) {
                     return res.status(500).send(err)
                 }
                 var mongo = require('mongodb');
                 var Grid = require('gridfs-stream');
                 const fs = require('fs');
-                // create or use an existing mongodb-native db instance
                 let mongo_client = await mongo_util.dbClient();
                 var GridFS = Grid(mongo_client, mongo);
-                let user_defined_error = require('../../utils/error');
-                // streaming to gridfs
-
                 var writestream = GridFS.createWriteStream({
                     filename: fileName
                 });
                 writestream.on('close', function (file) {
                     return (res.status(201).json({ message: `Image uploaded successfully with ObjectID : ${file}` }));
+
+                });
+                writestream.on('error', () => {
+                    return (res.status(500).json(user_defined_error.errorObject('Error uploading file.', 500)));
                 });
                 fs.createReadStream(fileName).pipe(writestream);
-
+                fs.unlink(fileName, (err) => {
+                    if (err) throw err;
+                    console.log(fileName, ' deleted');
+                });
             }
         )
 
