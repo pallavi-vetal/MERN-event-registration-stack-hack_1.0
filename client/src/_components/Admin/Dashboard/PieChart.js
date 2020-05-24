9faef39d5f93157964ed8016371bb439bdaeeb07
pallavi-vetal <vetalpallavi21@gmail.com>
2020-05-24 19:29:13 +0530

    improved pie chart

--- client/src/_components/Admin/Dashboard/PieChart.js
+++ client/src/_components/Admin/Dashboard/PieChart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-    PieChart, Pie, Cell,ResponsiveContainer
+    PieChart, Pie, Sector,ResponsiveContainer
 } from 'recharts';
 import Title from './Title';
 import { withStyles } from "@material-ui/core/styles";
@@ -13,27 +13,56 @@ const useStyles = theme => ({
   },
 });
 
-
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-    cx, cy, midAngle, innerRadius, outerRadius, percent, index,
-}) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
+const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+      fill, payload, percent, value } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? 'start' : 'end';
+  
     return (
-        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-            {`${(percent * 100).toFixed(0)}%`}
+      <g>
+        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 6}
+          outerRadius={outerRadius + 10}
+          fill={fill}
+        />
+        <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`Count ${value}`}</text>
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+          {`( ${(percent * 100).toFixed(2)}%)`}
         </text>
+      </g>
     );
-};
+  };
  class Example extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            activeIndex : 0
+        }
       }
     componentDidMount() {
         // If logged in and user navigates to Register page, should redirect them to dashboard
@@ -47,7 +76,11 @@ const renderCustomizedLabel = ({
        
       }
       
-
+      onPieEnter = (data, index) =>{
+        this.setState({
+          activeIndex: index,
+        });
+      }
     render() {
         let data = this.props.events.eventType.map((row=>{
             console.log(row)
@@ -61,21 +94,18 @@ const renderCustomizedLabel = ({
             <React.Fragment>
                 <Title>Registration Type</Title>
                 <ResponsiveContainer>
-                    <PieChart >
-                        <Pie
-                            data={data}
-                           
-                            labelLine={false}
-                            label={renderCustomizedLabel}
-                            outerRadius={80}
-                            fill="#8884d8"
-                            dataKey="value"
-                        >
-                            {
-                                data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-                            }
-                        </Pie>
-                    </PieChart>
+                <PieChart width={800} height={200}>
+        <Pie 
+        	activeIndex={this.state.activeIndex}
+          activeShape={renderActiveShape} 
+          data={data} 
+         
+          innerRadius={60}
+          outerRadius={80} 
+          fill="#8884d8"
+          onMouseEnter={this.onPieEnter}
+        />
+       </PieChart>
                 </ResponsiveContainer>
             </React.Fragment>
 
