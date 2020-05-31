11bca985da15a76f235c1e0ebdc77b143337fc93
omkarlanghe <langhe.omkar96@gmail.com>
2020-05-31 12:46:39 +0530

    server.js updated

--- server.js
+++ server.js
@@ -21,11 +21,15 @@ app.use(nocache());
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(body_parser.json());
 app.use(cors());
-app.use(fileUpload());  
+app.use(fileUpload());
 app.use(express.static(path.join(__dirname, "client", "build")))
 routes.navigateRoutes(app);
-const port = process.env.PORT || 8000 ;
+
+const port = process.env.PORT || 8000;
+
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-app.listen(port, () => console.log('Listening on Port',port));
+
+app.listen(port, () => console.log('Listening on Port', port));
